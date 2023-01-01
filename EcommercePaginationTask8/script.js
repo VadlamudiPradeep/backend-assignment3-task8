961e4cde0d963910b5ec98aacb1a5fd59020f8c9
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-01-01 09:19:00 +0530

    pagination->task8

--- EcommercePaginationTask8/script.js
+++ EcommercePaginationTask8/script.js
@@ -3,17 +3,18 @@ const parentContainer = document.getElementById("EcommerceContainer");
 const products=document.getElementById('Products')
 const qty=document.querySelector('.cart-number')
 const toast=document.getElementById('notification-container')
-const cart=document.getElementById('cart')
+const cart=document.getElementById('cart-holder')
 const seeCart=document.getElementById('open-cart')
 const closeCart=document.getElementById('cancel')
-const items=document.getElementsByClassName('cart')
+const items=document.querySelector('.cart-items')
 const pages=document.getElementById('pages-button')
+const orderUrl=`http://localhost:3000/orders`
+var totalPrice=0.00;
 
 //Event Listeners
 pages.addEventListener('click', showProducts);
-//cart.addEventListener('click' ,showCartProducts)
-products.addEventListener('click', loadCart)
-
+//pages.addEventListener('click', showCart);
+products.addEventListener('click', showCart);
 
 // from old .js
 parentContainer.addEventListener("click", (e) => {
@@ -61,8 +62,8 @@ if (e.target.className == "shop-item-button") {
 }
 if (e.target.className == "cart-btn-bottom" || e.target.className == "cart-bottom" || e.target.className == "cart-holder") {
   // update here for Task - 13 -> Show the Cart
-  const cartContainer = document.getElementById('cart');
-  cartContainer.innerHTML = ''
+  // const cartContainer = document.getElementById('cart');
+  // cartContainer.innerHTML = ''
   getCartDetails()
   // document.querySelector("#cart").style = "display:block;";
 }
@@ -82,7 +83,7 @@ if (e.target.className == "purchase-btn") {
 
 if (e.target.innerText == "REMOVE") {
   let total_cart_price = document.querySelector("#total-value").innerText;
-  total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2);
+  total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id}.cart-price`).innerText).toFixed(2);
   document.querySelector(".cart-number").innerText = parseInt(document.querySelector(".cart-number").innerText) - 1;
   document.querySelector("#total-value").innerText = `${total_cart_price.toFixed(2)}`;
   e.target.parentNode.parentNode.remove();
@@ -90,31 +91,10 @@ if (e.target.innerText == "REMOVE") {
 });
 // till from old .js
 
-// window.addEventListener('DOMContentLoaded', ()=>{
-//   axios.get('http://localhost:3000/products')
-//   .then((data)=>{
-//       console.log(data);
-//       if(data.request.status === 200){
-//           const products = data.data.products;
-//           var parentSection = document.getElementById('Products');
-//          products.forEach((product) => {
-//               var productHtml =`
-//               <div>
-//               <h1>${product.title}</h1>
-//               <img src=${product.imageUrl}></img>
-//               <button onClick='addToCart(${product.id})'>Add To Cart </button>
-//               </div>
-//               `
-//               parentSection.innerHTML = parentSection.innerHTML + productHtml
-//           });
-//       }
-//   })
-// });
 window.addEventListener("DOMContentLoaded", () => {
   console.log("loaded");
-  loadCart
+  showCart();
   showProducts()
- // showCartProducts();
 });
 
 function addToCart(productId) {
@@ -187,7 +167,9 @@ function showProducts(e){
           let h3=document.createElement('h3')
           h3.innerHTML=product.title
           let img=document.createElement('img')
-          img.src=product.imageUrl
+          img.src=product.imageUrl;
+          img.width = "200";
+          img.height = "200"
           let subdiv=document.createElement('div')
           subdiv.classList.add('product-details')
           let p=document.createElement('p')
@@ -241,7 +223,7 @@ function showProducts(e){
   }).catch(err=>console.log(err))
 }
 // Show Cart
-function loadCart(e){
+function showCart(e){
   let pageNo;
   try {
       pageNo=e.target.id
@@ -256,38 +238,33 @@ function loadCart(e){
   }).then(response => {
       console.log(response)
       qty.innerHTML=response.data.totalItems
-      items.innerHTML=""
       pages.innerHTML=""
-      response.data.products.forEach(item=>{
-        let h2 = document.createElement('h2');
-        h2.innerHTML = "CART"
-        let li=document.createElement('li')
+      // response.data.cartItems.map(item=>{
+        response.data.products.forEach((item) => {
+          let li=document.createElement('li')
           li.classList.add('item')
           let img=document.createElement('img')
-          img.src=item.imageUrl
+          img.src=item.imageUrl;
+          img.width = "50";
+          img.height= "50"
           let p1=document.createElement('p')
           p1.innerHTML=item.title
           let p2=document.createElement('p')
           p2.innerHTML=item.price
           let qty=document.createElement('input')
-          qty.classList.add('qty')
-          qty.type="number"
-          qty.value="1"
+          qty.classList.add('qty');
+          qty.type= "number"
+          qty.value= 1;
           let button=document.createElement('button')
           button.id=item.id
           button.innerHTML="REMOVE"
-          li.appendChild(h2);
           li.appendChild(img)
           li.appendChild(p1)
           li.appendChild(p2)
           li.appendChild(qty)
           li.appendChild(button)
-
-          items.appendChild(li);
-          document.body.appendChild(items)
-
+          items.appendChild(li)
       })
-   
 
       // Pagination Buttons for Cart
       let div=document.createElement('div')
@@ -307,8 +284,8 @@ function loadCart(e){
               cur_btn2.innerHTML=response.data.currentPage
               cur_btn2.id=response.data.currentPage
               cur_btn2.classList.add('cart-pages-active')
-              div.appendChild(cur_btn2);
-              
+              div.appendChild(cur_btn2)
+            
           }
           if(response.data.hasNextPage){
               let cur_btn2=document.createElement('button')
@@ -316,10 +293,10 @@ function loadCart(e){
               cur_btn2.id=response.data.nextPage
               cur_btn2.classList.add('cart-pages')
               div.appendChild(cur_btn2);
-            
+              
           }
   
-        items.appendChild(div)
+          items.appendChild(div)
       }
       else if(response.data.lastPage>2){
           if(response.data.currentPage==response.data.lastPage){
@@ -327,73 +304,109 @@ function loadCart(e){
               cur_btn.innerHTML=1
               cur_btn.id=1
               cur_btn.classList.add('cart-pages')
-              div.appendChild(cur_btn);
-              
+              div.appendChild(cur_btn)
           }
           if(response.data.hasPreviousPage){
               let cur_btn=document.createElement('button')
               cur_btn.innerHTML=response.data.previousPage
               cur_btn.id=response.data.previousPage
               cur_btn.classList.add('cart-pages')
-              div.appendChild(cur_btn);
-              
+              div.appendChild(cur_btn)
           }
           if(response.data.currentPage){
               let cur_btn2=document.createElement('button')
               cur_btn2.innerHTML=response.data.currentPage
               cur_btn2.id=response.data.currentPage
               cur_btn2.classList.add('cart-pages-active')
-              div.appendChild(cur_btn2);
-              
+              div.appendChild(cur_btn2)
           }
           if(response.data.hasNextPage){
               let cur_btn2=document.createElement('button')
               cur_btn2.innerHTML=response.data.nextPage
               cur_btn2.id=response.data.nextPage
               cur_btn2.classList.add('cart-pages')
-              div.appendChild(cur_btn2);
-            
+              div.appendChild(cur_btn2)
           }
           if(response.data.lastPage!==response.data.currentPage && !response.data.hasNextPage){
               let cur_btn2=document.createElement('button')
               cur_btn2.innerHTML=response.data.lastPage
               cur_btn2.id=response.data.lastPage
               cur_btn2.classList.add('cart-pages')
-              div.appendChild(cur_btn2);
-             
+              div.appendChild(cur_btn2)
           }
           if(response.data.currentPage==1){
               let cur_btn2=document.createElement('button')
               cur_btn2.innerHTML=response.data.lastPage
               cur_btn2.id=response.data.lastPage
               cur_btn2.classList.add('cart-pages')
-              div.appendChild(cur_btn2);
-             
+              div.appendChild(cur_btn2)
           }
-          items.innerHTML += div;
+          items.appendChild(div)
+      }else if(response.data.lastPage>2){
+        if(response.data.currentPage==response.data.lastPage){
+            let cur_btn=document.createElement('button')
+            cur_btn.innerHTML=1
+            cur_btn.id=1
+            cur_btn.classList.add('cart-pages')
+            div.appendChild(cur_btn)
+        }
+        if(response.data.hasPreviousPage){
+            let cur_btn=document.createElement('button')
+            cur_btn.innerHTML=response.data.previousPage
+            cur_btn.id=response.data.previousPage
+            cur_btn.classList.add('cart-pages')
+            div.appendChild(cur_btn)
+        }
+        if(response.data.currentPage){
+            let cur_btn2=document.createElement('button')
+            cur_btn2.innerHTML=response.data.currentPage
+            cur_btn2.id=response.data.currentPage
+            cur_btn2.classList.add('cart-pages-active')
+            div.appendChild(cur_btn2)
+        }
+        if(response.data.hasNextPage){
+            let cur_btn2=document.createElement('button')
+            cur_btn2.innerHTML=response.data.nextPage
+            cur_btn2.id=response.data.nextPage
+            cur_btn2.classList.add('cart-pages')
+            div.appendChild(cur_btn2)
+        }
+        if(response.data.lastPage!==response.data.currentPage && !response.data.hasNextPage){
+            let cur_btn2=document.createElement('button')
+            cur_btn2.innerHTML=response.data.lastPage
+            cur_btn2.id=response.data.lastPage
+            cur_btn2.classList.add('cart-pages')
+            div.appendChild(cur_btn2)
+        }
+        if(response.data.currentPage==1){
+            let cur_btn2=document.createElement('button')
+            cur_btn2.innerHTML=response.data.lastPage
+            cur_btn2.id=response.data.lastPage
+            cur_btn2.classList.add('cart-pages')
+            div.appendChild(cur_btn2)
+        }
+        items.appendChild(div)
       }
 
-      let p=document.createElement('p')
-      p.classList.add('total')
-      p.innerHTML="Total "
-      let total=document.createElement('span')
-      total.id="total"
-      totalPrice=response.data.totalPrice
-      total.innerHTML=`₹ ${parseFloat(response.data.totalPrice).toFixed(2)}`
-      p.appendChild(total)
-      items.appendChild(p);
-      let button=document.createElement('button')
-      button.classList.add('purchase')
-      button.innerHTML="ORDER NOW"
-      items.appendChild(button);
+      // let p=document.createElement('p')
+      // p.classList.add('total')
+      // p.innerHTML="Total "
+      // let total=document.createElement('span')
+      // total.id="total"
+      // totalPrice=response.data.totalPrice
+      // total.innerHTML=`₹ ${parseFloat(response.data.totalPrice).toFixed(2)}`
+      // p.appendChild(total)
+      // items.appendChild(p)
+      // let button=document.createElement('button')
+      // button.classList.add('purchase')
+      // button.innerHTML="ORDER NOW"
+      // items.appendChild(button)
 
       // const order=document.querySelector('.purchase')
       // order.addEventListener('click', createOrder)
   })
-   .catch(err=>console.log(err))
-   //.then(()=>{
-  //   const cart_pages=document.querySelector('.pages-container')
-  //   cart_pages.addEventListener('click', loadCart)
-  // })
+  .catch(err=>console.log(err)).then(()=>{
+      const cart_pages=document.querySelector('.pages-container')
+      cart_pages.addEventListener('click', showCart)
+  })
 }
-
